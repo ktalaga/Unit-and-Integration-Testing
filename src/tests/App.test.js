c2a07366cc8c2e6d9ac6cb53269377450207de0a
konrad <ktalaga90@gmail.com>
2021-05-15 00:39:07 +0100

    first test passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,7 +12,20 @@ describe('Calculator', () => {
     const runningTotal = container.getByTestId('running-total');
     fireEvent.click(button4);
     expect(runningTotal).toHaveTextContent('4');
-    
   })
+
+  it('should return 5 when 1 is added to 4', () => {
+    const button_1 = container.getByTestId('number1');
+    const button_4 = container.getByTestId('number4');
+    const button_add = container.getByTestId('add');
+    const equals = container.getByTestId('equals')
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button_1);
+    fireEvent.click(button_add);
+    fireEvent.click(button_4);
+    fireEvent.click(equals)
+    expect(runningTotal).toHaveTextContent('5');
+  })
+
 })
 
